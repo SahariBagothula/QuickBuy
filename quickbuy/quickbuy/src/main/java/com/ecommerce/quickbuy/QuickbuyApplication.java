5f01e2603d03f0394a217d3e649bb1d030aea058
SahariBagothula <121758237+SahariBagothula@users.noreply.github.com>
2024-10-22 10:10:40 +0530

    started building the backend

---
+++ quickbuy/quickbuy/src/main/java/com/ecommerce/quickbuy/QuickbuyApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.quickbuy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickbuyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickbuyApplication.class, args);
+	}
+
+}
---
+++ quickbuy/quickbuy/src/test/java/com/ecommerce/quickbuy/QuickbuyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.quickbuy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickbuyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
